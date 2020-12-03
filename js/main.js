5a339d8066ee4ee523c2a854c3a421aab663fb13
Karina Karpenko <karinakarpenko.k@gmail.com>
2020-12-03 17:19:51 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -22,8 +22,19 @@ $(document).on('click','.form-cancel',function(){
 $(document).ready(function() {
     $('#adaptive').lightSlider({
         adaptiveHeight:true,
+        auto:true,
         item:1,
         slideMargin:0,
         loop:true
+    });
+});
+
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cs-hidden');
+        }
     });
 });
